48e973bddce229ed9267ce90de3a46a982604fbd
Aisha <kalimatovaaisha@mail.ru>
2023-05-15 17:02:23 +0600

    adding documentation to inner class

--- MyHashTable.java
+++ MyHashTable.java
@@ -3,19 +3,40 @@ public class MyHashTable <K, V>{
         private K key;
         private V value;
         private HashNode<K, V> next;
+        /**
+         * HashNode - constructor to set values in fields
+         * @param key - given key to set
+         * @param value - given value to set
+         */
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+        /**
+         * getKey - method that returns key of node
+         * @return K - key type
+         */
         public K getKey() {
             return key;
         }
+        /**
+         * getValue - method that returns value of node
+         * @return V - value type
+         */
         public V getValue() {
             return value;
         }
+        /**
+         * setValue - method that set value in node
+         * @param value - given value to set
+         */
         public void setValue(V value) {
             this.value = value;
         }
+        /**
+         * toString - method that returns node with specific type
+         * @return String
+         */
         public String toString() {
             return "{" + key + " " + value + "}";
         }
